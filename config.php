32de7df9662f72e277f9d6734d9dd94b16d4788c
Guillaume Bellemare <gbellemare@propagandadesign.com>
2017-10-24 11:03:53 -0400

    asdfasdf

---
+++ config.php
@@ -0,0 +1,31 @@
+  
+<?php
+// HTTP
+
+define('HTTP_SERVER', 'http://espacedev1.com/sap/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://espacedev1.com/sap/');
+
+// DIR
+
+define('DIR_APPLICATION', '/home/espacedev1/public_html/sap/catalog/');
+define('DIR_SYSTEM', '/home/espacedev1/public_html/sap/system/');
+define('DIR_LANGUAGE', '/home/espacedev1/public_html/sap/catalog/language/');
+define('DIR_TEMPLATE', '/home/espacedev1/public_html/sap/catalog/view/theme/');
+define('DIR_CONFIG', '/home/espacedev1/public_html/sap/system/config/');
+define('DIR_IMAGE', '/home/espacedev1/public_html/sap/image/');
+define('DIR_CACHE', '/home/espacedev1/public_html/sap/system/cache/');
+define('DIR_DOWNLOAD', '/home/espacedev1/public_html/sap/system/download/');
+define('DIR_UPLOAD', '/home/espacedev1/public_html/sap/system/upload/');
+define('DIR_MODIFICATION', '/home/espacedev1/public_html/sap/system/modification/');
+define('DIR_LOGS', '/home/espacedev1/public_html/sap/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'espacedevsap');
+define('DB_PASSWORD', 'Soq3VFPREXg7');
+define('DB_DATABASE', 'surpluspont');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
